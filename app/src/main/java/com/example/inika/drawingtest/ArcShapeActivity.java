2cd93ef76c3e08780007ab6869017602ec40b7c3
artdev1743 <artemtsurkan002@gmail.com>
2022-07-08 19:20:50 -0400

    HW

---
+++ app/src/main/java/com/example/inika/drawingtest/ArcShapeActivity.java
@@ -0,0 +1,37 @@
+package com.example.inika.drawingtest;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.drawable.ShapeDrawable;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+
+public class ArcShapeActivity extends DrawingActivity{
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_arc_shape);
+        setContentView(new ArcShapeActivity.ViewWithArc(this));
+    }
+
+    private static class ViewWithArc extends View {
+
+        public ViewWithArc(Context context) {
+            super(context);
+        }
+
+        @Override protected void onDraw(Canvas canvas) {
+            ShapeDrawable pacMan = new ShapeDrawable(new ArcShapeActivity(0, 345));
+            pacMan.setIntrinsicHeight(100);
+            pacMan.setIntrinsicWidth(100);
+            pacMan.getPaint().setColor(Color.MAGENTA);
+            ImageView iView = (ImageView)findViewById(R.id.ImageView1);
+            iView.setImageDrawable(pacMan);
+        }
+    }
+}
